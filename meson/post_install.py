9b9e17b9f55c9a554ae960fe1e5a4abb75a12d75
Raí B. Toffoletto <rai@toffoletto.me>
2021-09-07 05:41:15 +0000

    DRY Use prefix defined in variable

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,7 +4,7 @@ from os import path, environ
 import subprocess
 
 prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+schemadir = path.join(prefix, 'share', 'glib-2.0', 'schemas')
 datadir = path.join(prefix, 'share')
 desktop_database_dir = path.join(datadir, 'applications')
 
@@ -14,4 +14,5 @@ if not environ.get('DESTDIR'):
     print('Updating desktop database…')
     subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
     print('Updating icon cache…')
-    subprocess.call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+    subprocess.call(['gtk-update-icon-cache', '-qtf',
+                    path.join(datadir, 'icons', 'hicolor')])
